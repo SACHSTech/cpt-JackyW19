035eb45bc4032543a3ef36044f2ab153f39e0762
JackyW19 <jacky.wang23@ycdsbk12.ca>
2023-01-26 01:07:23 -0500

    edited database

--- src/cpt/DataBase.java
+++ src/cpt/DataBase.java
@@ -8,45 +8,21 @@ public class DataBase{
     private double off;
     private double def;
     private double spi;
-    private int addingTag;
+
     
 
     /**
      * starts of datapoint tag as 0
      */
-    public DataBase() {
-        this.addingTag = 0;
+    public DataBase(int rank, String name, String confed, Double off, Double def, Double spi) {
+        this.rank = rank;
+        this.name = name;
+        this.confed = confed;
+        this.off = off;
+        this.def = def;
+        this.spi = spi;
     }
 
-    /**
-     * adds value to properties based on previously added values
-     * @param value
-     */
-    public void add(String value) {
-        switch(addingTag) {
-            case 0:
-                this.rank = Integer.parseInt(value);
-                break;
-            case 1:
-                this.name = value;
-                break;
-            case 2:
-                this.confed = value;
-                break;
-            case 3:
-                this.off = Double.parseDouble(value);
-                break;
-            case 4:
-                this.def = Double.parseDouble(value);
-                break;
-            case 5:
-                this.spi = Double.parseDouble(value);
-                break;
-            
-        }
-
-        this.addingTag++;
-    }
 
     // getter methods
 
