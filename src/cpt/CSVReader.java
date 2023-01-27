e2b1199d350caf95bc2d2a4e047d224f5e25f62a
JackyW19 <jacky.wang23@ycdsbk12.ca>
2023-01-26 23:06:40 -0500

    fixed CSVReader

--- src/cpt/CSVReader.java
+++ src/cpt/CSVReader.java
@@ -1,23 +1,39 @@
 package cpt;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.sql.DatabaseMetaData;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.io.IOException;
 
 public class CSVReader {
-    public static List<String[]> readCSV(String fileName) {
-        List<String[]> data = new ArrayList<>();
+    public ArrayList<DataBase[]> readCSV(String fileName) {
+        ArrayList<DataBase[]> data = new ArrayList<>();
         String line;
         // buffered reader 
-        try (BufferedReader br = new BufferedReader(new FileReader("src/cpt/DataBase.java"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             while ((line = br.readLine()) != null) {
-                String[] lineData = line.split(",");
-                data.add(lineData);
+
+                String[] values = line.split(",");
+                int rank = Integer.parseInt(values[0]);
+                String name = values[1];
+                String confed = values[2];
+                Double off = Double.parseDouble(values[3]);
+                Double def = Double.parseDouble(values[4]);
+                Double spi = Double.parseDouble(values[5]);
+
+                // Create new object and add to arraylist 
+                data.add(new DataBase[]{new DataBase(rank, name, confed, off, def,  spi)});
+               
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return data;
     }
-}
 
+    public List<DataBase> CSVread(String string) {
+        return null;
+    }
+}
