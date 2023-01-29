672d1d9437fa3c5f710dab554f76dc6e63514893
JackyW19 <jacky.wang23@ycdsbk12.ca>
2023-01-28 20:01:19 -0500

    added checkboxes to filter to one country for bar

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -1,6 +1,8 @@
 package cpt;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -12,6 +14,7 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -28,7 +31,7 @@ public class Main extends Application {
 public void start(Stage stage) {
 
     // Create the x-axis for scatter chart
-    NumberAxis xAxis = new NumberAxis("Ranking", 0.0d,85.0d, 1.0d );
+    NumberAxis xAxis = new NumberAxis("Ranking", 0.0d,81.0d, 1.0d );
 
     // Create the y-axis for scatter chart
     NumberAxis yAxis = new NumberAxis("SPI Rating", 0.0d, 100.0d, 10.0d);
@@ -80,12 +83,23 @@ public void start(Stage stage) {
     }
 
    
-    Button resetBtn = new Button("Reset Data");
-    nameCheckBoxes.add(resetBtn, 4, row + 2);
 
-    resetBtn.setOnAction(event -> {
+
+    
+    Button updateButton = new Button("Reset Button");
+    nameCheckBoxes.add(updateButton, 4, row + 2);
+    updateButton.setOnAction(new EventHandler<ActionEvent>() {
+    @Override
+    public void handle(ActionEvent event) {
+        // Clear the current data from the plot
         scatterChart.getData().clear();
-    });
+
+        // Add the original data to the plot
+        for (DataBase d : dataBaseList) {
+            data.getData().add(new XYChart.Data<>(d.getRank(), d.getSpi()));
+        }
+    }
+});
 
 
     // Add the data series to the scatter chart
@@ -105,8 +119,11 @@ public void start(Stage stage) {
                 scatterChart.getData().clear();
                 scatterChart.getData().add(filteredData);
             });
+            }
+
+            
         }
-    }
+    
     
     // Create the x-axis for bar chart
     CategoryAxis xAxisBar = new CategoryAxis();
@@ -137,6 +154,31 @@ public void start(Stage stage) {
         }
 
         barChart.getData().addAll(data2, data3);
+        
+         // Add an event handler to each checkbox that updates the scatter plot data when the box is checked or unchecked
+    for (Node node : nameCheckBoxes.getChildren()) {
+        if (node instanceof CheckBox) {
+            CheckBox cb = (CheckBox) node;
+            cb.setOnAction(event -> {
+                XYChart.Series<String, Number> filteredData2 = new XYChart.Series<>();
+                XYChart.Series<String, Number> filteredData3 = new XYChart.Series<>();
+                for (DataBase d : dataBaseList) {
+                    if (cb.isSelected() && cb.getText().equals(d.getName())) {
+                        filteredData2.getData().add(new XYChart.Data<>(d.getName(), d.getOff()));
+                        filteredData3.getData().add(new XYChart.Data<>(d.getName(), d.getDef()));
+                        filteredData2.setName("Offensive Rating");
+                        filteredData3.setName("Defensive Rating");
+                    }
+                }
+                barChart.getData().clear();
+                barChart.getData().addAll(filteredData2, filteredData3);
+            });
+            }
+
+    }
+        // set sizing of the barchart
+        barChart.setPrefWidth(600);
+        barChart.setPrefHeight(450);
 
         // Add the checkboxes and scatter chart to the grid
         nameCheckBoxes.add(scatterChart, 0, row + 1, 3, 1);
@@ -153,7 +195,7 @@ public void start(Stage stage) {
         chartContainer.getChildren().add(nameCheckBoxes);
 
         // Create the scene
-        Scene scene = new Scene(chartContainer, 1200, 1000);
+        Scene scene = new Scene(chartContainer, 1200, 1200);
 
         // Set the stage
         stage.setTitle("Country's SPI Global Ranking");
